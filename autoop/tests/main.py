51a01dc5c39860bf0bf02207d3111069bf9e3c3b
TijnKrikke <Tijnkrikke@gmail.com>
2024-11-15 15:58:03 +0100

    Added a piece of code to avoid flake8 errors, it is not pretty but we value having no flake8 style errors more than the small amount of memory used.

--- autoop/tests/main.py
+++ autoop/tests/main.py
@@ -4,5 +4,12 @@ from autoop.tests.test_storage import TestStorage
 from autoop.tests.test_features import TestFeatures
 from autoop.tests.test_pipeline import TestPipeline
 
+# to avoid flake8 style errors, sorry in advance
+if 1 + 1 == 3:
+    t = TestDatabase()
+    tr = TestStorage()
+    trt = TestFeatures()
+    trtr = TestPipeline()
+
 if __name__ == '__main__':
     unittest.main()
